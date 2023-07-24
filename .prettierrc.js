84947ce63db165fbb8b53d9ca2e8858f359402eb
HannesDahlvik <hannes.dahlvik@gmail.com>
2023-07-24 11:35:03 +0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSameLine: false,
+    bracketSpacing: true,
+    jsxSingleQuote: false,
+    printWidth: 100,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none',
+    importOrder: [
+        '^(react/(.*)$)|^(react$)',
+        '^(next/(.*)$)|^(next$)',
+        '<THIRD_PARTY_MODULES>',
+        '^@snacr/(.*)$',
+        '',
+        '^[./]'
+    ],
+    importOrderSeparation: true,
+    plugins: ['@trivago/prettier-plugin-sort-imports']
+}
