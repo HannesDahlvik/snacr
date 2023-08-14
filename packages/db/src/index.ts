667665cc9cb1097e68e2c33dee1daf9aba23ebdc
HannesDahlvik <hannes.dahlvik@gmail.com>
2023-08-14 13:20:51 +0300

    fix: Remove db pool port for prod

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -6,8 +6,7 @@ export const pool = new Pool({
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT as unknown as number
+    password: process.env.DATABASE_PASSWORD
 })
 
 const dialect = new PostgresDialect({
